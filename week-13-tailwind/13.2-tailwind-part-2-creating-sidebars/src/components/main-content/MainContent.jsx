43a29a4ad9dd435a8f820941dec54b24f4213d43
Shivam Rana <4826shivam@gmail.com>
2025-07-08 02:03:30 +0530

    week-13

--- week-13-tailwind/13.2-tailwind-part-2-creating-sidebars/src/components/main-content/MainContent.jsx
+++ week-13-tailwind/13.2-tailwind-part-2-creating-sidebars/src/components/main-content/MainContent.jsx
@@ -1,11 +1,11 @@
 export const MainContent = () => {
   return (
     <div className="w-full">
-      <div className="h-40 bg-black"></div>
-      <div className="w-full grid grid-cols-10 gap-6 mx-10">
-        <div className="h-80 bg-red-200 rounded-2xl col-span-2 -translate-y-12 shadow-lg"></div>
-        <div className="h-80 bg-green-200 rounded-2xl col-span-5 mt-24 shadow-lg"></div>
-        <div className="h-80 bg-yellow-200 rounded-2xl col-span-3 mt-24 shadow-lg"></div>
+      <div className="h-40 bg-black hidden md:block"></div>
+      <div className="w-full grid grid-cols-10 gap-6 p-8">
+        <div className="h-80 bg-red-200 rounded-2xl col-span-10 hidden md:block md:col-span-2 -translate-y-24 shadow-lg "></div>
+        <div className="h-80 bg-green-200 rounded-2xl col-span-10 md:col-span-5 shadow-lg"></div>
+        <div className="h-80 bg-yellow-200 rounded-2xl col-span-10 md:col-span-3 shadow-lg"></div>
       </div>
     </div>
   );
--- week-13-tailwind/13.2-tailwind-part-2-creating-sidebars/src/components/sidebars/Sidebar4.jsx
+++ week-13-tailwind/13.2-tailwind-part-2-creating-sidebars/src/components/sidebars/Sidebar4.jsx
@@ -1,3 +1,57 @@
-export const Sidebar4 = ({ isSidebarOpen, setIsSidebarOpen }) => {
-  return <div className="w-80 h-screen bg-red-100 top-0 left-0"></div>;
+import { useState } from "react";
+import { SidebarIcon } from "../icons/SidebarIcon";
+import { useEffect } from "react";
+
+export const Sidebar4 = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  useEffect(() => {
+    if (isDesktop == false) {
+      setIsSidebarOpen(false);
+    } else {
+      setIsSidebarOpen(true);
+    }
+  }, [isDesktop]);
+
+  if (!isSidebarOpen) {
+    return (
+      <div className="fixed top-0 left-0">
+        <div
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="cursor-pointer hover:bg-slate-200"
+        >
+          <SidebarIcon />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-80 h-screen top-0 left-0 bg-red-100 absolute md:relative">
+        <div
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="cursor-pointer hover:bg-slate-200"
+        >
+          <SidebarIcon />
+        </div>
+      </div>
+    );
+  }
+};
+
+const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
 };
