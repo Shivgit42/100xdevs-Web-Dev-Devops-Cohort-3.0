c39f299df3efff916a0c2f03007f78a1b6593e24
Shivam Rana <4826shivam@gmail.com>
2025-08-14 11:04:35 +0530

    week-20

--- week-20-nextjs-continuation/20.2-nextjs-auth/lecture-codes/02-auth-using-next-auth/app/api/auth/[...nextauth]/route.ts
+++ week-20-nextjs-continuation/20.2-nextjs-auth/lecture-codes/02-auth-using-next-auth/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
-      name: "Login with email",
+      name: "email",
       credentials: {
         username: { label: "Username", type: "text", placeholder: "shivam" },
         password: { label: "Password", type: "password" },
@@ -13,10 +14,12 @@ const handler = NextAuth({
         const username = credentials?.username;
         const password = credentials?.password;
 
+        console.log(username, password);
+
         const user = {
           id: "1",
           name: "shivam",
-          username: "shivam",
+          email: "shivam@gmail.com",
         };
 
         if (user) {
@@ -26,7 +29,12 @@ const handler = NextAuth({
         }
       },
     }),
+    GoogleProvider({
+      clientId: process.env.CLIENT_ID!,
+      clientSecret: process.env.CLIENT_SECRET!,
+    }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
--- week-20-nextjs-continuation/20.2-nextjs-auth/lecture-codes/02-auth-using-next-auth/app/page.tsx
+++ week-20-nextjs-continuation/20.2-nextjs-auth/lecture-codes/02-auth-using-next-auth/app/page.tsx
@@ -1,17 +1,34 @@
-"use client";
+// "use client";
 
-import { SessionProvider, useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
-  return (
-    <SessionProvider>
-      <RealHome />
-    </SessionProvider>
-  );
-}
+// import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
 
-function RealHome() {
-  const session = useSession();
+// export default function Home() {
+//   return (
+//     <SessionProvider>
+//       <RealHome />
+//     </SessionProvider>
+//   );
+// }
 
-  return <div>{session.status === "authenticated" ? "Logout" : "Sign in"}</div>;
+// function RealHome() {
+//   const session = useSession();
+
+//   return (
+//     <div>
+//       {session.status === "authenticated" && (
+//         <button onClick={() => signOut()}>Logout</button>
+//       )}
+//       {session.status === "unauthenticated" && (
+//         <button onClick={() => signIn()}>Sign In</button>
+//       )}
+//     </div>
+//   );
+// }
+
+export default async function Home() {
+  const session = await getServerSession();
+
+  return <div>{JSON.stringify(session)}</div>;
 }
