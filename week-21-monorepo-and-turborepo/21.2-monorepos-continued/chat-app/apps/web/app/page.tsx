51e2e0baefdee7f8fab2cddadffeddf8092a9613
Turbobot <turbobot@vercel.com>
2025-08-15 17:13:59 +0530

    feat(create-turbo): apply official-starter transform

--- week-21-monorepo-and-turborepo/21.2-monorepos-continued/chat-app/apps/web/app/page.tsx
+++ week-21-monorepo-and-turborepo/21.2-monorepos-continued/chat-app/apps/web/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             rel="noopener noreferrer"
             className={styles.secondary}
           >
-            Read my docs
+            Read our docs
           </a>
         </div>
         <Button appName="web" className={styles.secondary}>
