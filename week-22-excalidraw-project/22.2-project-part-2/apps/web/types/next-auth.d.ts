3a7c559cfa2719ca5752e822ac9c0aacedf0b831
Shivam Rana <4826shivam@gmail.com>
2025-09-16 19:14:46 +0530

    week-22

---
+++ week-22-excalidraw-project/22.2-project-part-2/apps/web/types/next-auth.d.ts
@@ -0,0 +1,29 @@
+import { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    id?: string;
+    name?: string;
+    email?: string;
+    image?: string;
+  }
+  interface Session {
+    accessToken?: string;
+    user: {
+      id?: string;
+      email?: string;
+      name?: string;
+      image?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id?: string;
+    image?: string;
+    email?: string;
+    name?: string;
+    picture?: string;
+  }
+}
